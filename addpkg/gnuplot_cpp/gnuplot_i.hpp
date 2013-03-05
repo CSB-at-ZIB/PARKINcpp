7e3a32560567c06dec8ff5a5fcb3a137cdb22a46
Thomas Dierkes <tom.dierkes@web.de>
2013-03-05 15:32:30 +0100

    Path adjusted in additional gnuplot packages (win)

--- addpkg/gnuplot_cpp/gnuplot_i.hpp
+++ addpkg/gnuplot_cpp/gnuplot_i.hpp
@@ -591,7 +591,7 @@ int Gnuplot::tmpfile_num = 0;
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__)
 std::string Gnuplot::m_sGNUPlotFileName = "pgnuplot.exe";
-std::string Gnuplot::m_sGNUPlotPath = "C:/program files/gnuplot/bin/";
+std::string Gnuplot::m_sGNUPlotPath = "C:/program_files/gnuplot/bin/";
 #elif defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
 std::string Gnuplot::m_sGNUPlotFileName = "gnuplot";
 std::string Gnuplot::m_sGNUPlotPath = "/usr/local/bin/";
